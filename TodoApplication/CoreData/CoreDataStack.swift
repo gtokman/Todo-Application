b3ec1923d05d5fa312bff54b09e0abaa082a7d0d
Gary Guerman Tokman <gtokman@fullsail.edu>
2017-11-20 18:21:53 -0500

    finished project

---
+++ TodoApplication/CoreData/CoreDataStack.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataStack.swift
+//  TodoApplication
+//
+//  Created by Gary Tokman on 11/15/17.
+//  Copyright © 2017 Gary Tokman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    var container: NSPersistentContainer {
+        let container = NSPersistentContainer(name: "Todos")
+        container.loadPersistentStores { (description, error) in
+            guard error == nil else {
+                print("Error: \(error!)")
+                return
+            }
+        }
+        
+        return container
+    }
+
+    var managedContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
---
+++ TodoApplication/VC/AddTodoViewController.swift
@@ -0,0 +1,128 @@
+//
+//  AddTodoViewController.swift
+//  TodoApplication
+//
+//  Created by Gary Tokman on 11/12/17.
+//  Copyright © 2017 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddTodoViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var managedContext: NSManagedObjectContext!
+    var todo: Todo?
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(with:)),
+            name: .UIKeyboardWillShow,
+            object: nil
+        )
+        textView.becomeFirstResponder()
+        
+        if let todo = todo {
+            textView.text = todo.title
+            textView.text = todo.title
+            segmentedControl.selectedSegmentIndex = Int(todo.priotity)
+        }
+    }
+    
+    // MARK: Actions
+    
+    @objc func keyboardWillShow(with notification: Notification) {
+        let key = "UIKeyboardFrameEndUserInfoKey"
+        guard let keyboardFrame = notification.userInfo?[key] as? NSValue else { return }
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.height + 16
+        
+        bottomConstraint.constant = keyboardHeight
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    fileprivate func dismissAndResign() {
+        dismiss(animated: true)
+        textView.resignFirstResponder()
+    }
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        dismissAndResign()
+    }
+    
+    @IBAction func done(_ sender: UIButton) {
+        guard let title = textView.text, !title.isEmpty else {
+            return
+        }
+       
+        if let todo = self.todo {
+            todo.title = title
+            todo.priotity = Int16(segmentedControl.selectedSegmentIndex)
+        } else {
+            let todo = Todo(context: managedContext)
+            todo.title = title
+            todo.priotity = Int16(segmentedControl.selectedSegmentIndex)
+            todo.date = Date()
+        }
+        
+        do {
+            try managedContext.save()
+            dismissAndResign()
+        } catch {
+            print("Error saving todo: \(error)")
+        }
+    }
+}
+
+extension AddTodoViewController: UITextViewDelegate {
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if doneButton.isHidden {
+            textView.text.removeAll()
+            textView.textColor = .white
+            
+            doneButton.isHidden = false
+            
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
